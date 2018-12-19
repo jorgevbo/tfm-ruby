f121689beee591ce66bb0cc080e031b0dcd5b87a
Jorge Vasquez <jorgev.bo@gmail.com>
2018-12-19 15:33:43 -0400

    Configurar listado de competencias

--- app/admin/competencies.rb
+++ app/admin/competencies.rb
@@ -2,7 +2,6 @@ ActiveAdmin.register Competency do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :name, :competency_framework_id
 #
 # or
 #
@@ -11,5 +10,19 @@ ActiveAdmin.register Competency do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  actions :index, :show
 
+  filter :name, label: 'Nombre'
+  filter :competency_framework, label: 'Marco de competencia'
+  filter :moodle_id, label: 'ID Moodle'
+  filter :moodle_idnumber, label: 'ID Number Moodle'
+
+  index do
+    id_column
+    column 'Nombre', :name
+    column 'Marco de competencia', :competency_framework
+    column 'ID Moodle', :moodle_id
+    column 'ID Number Moodle', :moodle_idnumber
+    actions
+  end
 end
