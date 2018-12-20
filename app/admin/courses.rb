2120ca31b5e00064454ce42261271b7b95352714
Jorge Vasquez Cayoja <jorge.vasquez@unir.net>
2018-12-20 11:01:27 -0400

    personalizar index de courses

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -12,4 +12,22 @@ ActiveAdmin.register Course do
 #   permitted
 # end
   actions :index, :show
+
+  filter :name, label: 'Nombre'
+  filter :startdate, label: 'Fecha de inicio'
+  filter :enddate, label: 'Fecha de fin'
+  filter :moodle_id, label: 'ID Moodle'
+  filter :moodle_shortname, label: 'Nombre Corto'
+
+  index do
+    id_column
+    column 'Nombre', :name
+    column 'Fecha de inicio', :startdate
+    column 'Fecha de Fin', :enddate
+    column 'ID Moodle', :moodle_id
+    column 'Nombre Corto', :moodle_shortname
+    column 'Nombre Completo', :moodle_fullname
+
+    actions
+  end
 end
