8059fcbdcb550fdc7478130c58c5d8894a66a851
Jorge Vasquez Cayoja <jorge.vasquez@unir.net>
2018-12-31 12:16:21 -0400

    agregar accion para ejecutar migracion

--- app/admin/migrate.rb
+++ app/admin/migrate.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register_page 'Migrate' do
+
+  page_action :execute, method: :post do
+    operations = MoodleMigrator.start
+    flash[:notice] = "El proceso se ha completado en #{operations[:seconds].to_i} seg."
+    redirect_to admin_migrate_path
+  end
+
   content do
     render partial: 'index'
+  end
+
+  sidebar 'Ayuda' do
+    "Una vez iniciado el proceso, por favor espere a que la página se recargue automáticamente"
   end
 end
