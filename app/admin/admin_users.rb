b43d6060f2d4a354594171faa4cdac6a3cf5513d
Jorge Vasquez Cayoja <jorge.vasquez@unir.net>
2019-01-02 12:36:22 -0400

    validaciones de edicion de usuario

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation, :role
+  permit_params :email, :password, :password_confirmation, :role_id
 
   index do
     selectable_column
@@ -28,4 +28,10 @@ ActiveAdmin.register AdminUser do
     f.actions
   end
 
+  controller do
+    def update_resource(object, attributes)
+      update_method = attributes.first[:password].present? ? :update_attributes : :update_without_password
+      object.send(update_method, *attributes)
+    end
+  end
 end
